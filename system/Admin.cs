3aba6b0d2949443001e10e484ce53a74af2ea20b
Mohamed Refaat <63193470+moRefaat271@users.noreply.github.com>
2023-01-15 21:47:47 +0200

    Add files via upload

---
+++ system/Admin.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace system
+{
+    internal class Admin : User
+    {
+        public Admin(int SSN, string username, string password, bool auth) :base(SSN, username, password, auth) {}
+
+        /*
+static List<Train> trains = new List<Train>();
+static List<Station> staions = new List<Staion>();
+static List<OlinePassenger> olinePassengers = new List<OlinePassenger>();
+static List<Trip> trips = new List<Trip>();*/
+
+        static List<Employee> employees = new List<Employee>();
+        
+
+
+    }
+}
