33dd3cf3d3575d27a429198b1bfc2c34543d5fab
anazhmetdin <anazhmetdin@gmail.com>
2023-01-17 06:42:32 +0200

    add login/signup passenger menus

--- system/Program.cs
+++ system/Program.cs
@@ -1,105 +1,177 @@
-﻿using system;
-using System;
-using System.Runtime.Intrinsics.X86;
+﻿using System;
 
 namespace system
 {
     internal class Program
     {
+        private static readonly Dictionary<string, string> menues = new()
+        {
+            {"OnlinePassengerLanding",
+                "1- Login\n"+
+                "2- Signup\n"+
+                "3- View Trips\n"+
+                "0- Exit"},
 
+            {"login",
+                "usernaem:" }
+        };
 
+        private static void PrintMenu(string menuName)
+        {
+            Console.Clear();
+            Console.WriteLine(menues[menuName]);
+        }
 
+        private static int GetUserOption(string menuName, int low, int high)
+        {
+            int option = -1;
 
-        /*    private static Dictionary<string, string> menues = new()
+            try
             {
-                {"main",
-                    "1- login\n"+
-                    "2- signup\n"+
-                    "0- exit"},
+                PrintMenu(menuName);
+                Console.WriteLine("\nPlease enter a value between {0} and {1}", low, high);
+                option = Convert.ToInt32(Console.ReadLine());
+            }
+            catch { }
+            
 
-                {"login",
-                    "usernaem:" }
-            };
-
-            private static void PrintMenu(string menuName)
+            while (option < low || option > high)
             {
-                Console.WriteLine(menues[menuName]);
+                try
+                {
+                    PrintMenu(menuName);
+                    Console.WriteLine("\nInvalid option, please enter a value between {0} and {1}", low, high);
+                    option = Convert.ToInt32(Console.ReadLine());
+                }
+                catch {}
             }
 
-            private static int GetUserOption(int low, int high)
+            return option;
+        }
+
+        private static OnlinePassenger? loginPassenger()
+        {
+            string? username, password;
+            char key = '\0';
+            OnlinePassenger? passenger = null;
+
+            while (passenger == null && key != '0')
             {
-                int option;
+                Console.Clear();
+                Console.WriteLine("Login\n\n");
 
-                Console.WriteLine("Please enter a value between {0} and {1}", low, high);
-                option = Console.ReadLine(); // convert to integer
+                Console.WriteLine("Username: ");
+                username = Console.ReadLine();
 
-                while (option < low || option < high)
+                Console.WriteLine("\nPassword: ");
+                password = Console.ReadLine();
+
+                passenger = Admin.loginPassenger(username!, password!);
+
+                if (passenger == null)
                 {
-                    Console.WriteLine("Invalid option, please enter a value between {0} and {1}", low, high);
+                    Console.WriteLine("\nWrong username or password, enter 0 to go back, or any key to try again");
+                    key = Console.ReadKey().KeyChar;
+                }
+            }
+
+            return passenger;
+        }
+
+        private static OnlinePassenger? signupPassenger()
+        {
+            string? username, password;
+            char key = '\0';
+            int SSN = 0;
+            OnlinePassenger? passenger = null;
+
+            while (passenger == null && key != '0')
+            {
+                Console.Clear();
+                Console.WriteLine("Sign up\n\n");
+
+                Console.WriteLine("Username:");
+                username = Console.ReadLine();
+
+                Console.WriteLine("\nPassword:");
+                password = Console.ReadLine();
+
+                try
+                {
+                    Console.WriteLine("\nSSN:");
+                    SSN = Convert.ToInt32(Console.ReadLine());
+                } catch {
+                    Console.WriteLine("\nInvalid SSN, enter 0 to go back, or any key to try again");
+                    key = Console.ReadKey().KeyChar;
+                    continue;
                 }
 
-                return option;
-            }*/
+                passenger = OnlinePassenger.signup(SSN, username!, password!);
+
+                if (passenger == null)
+                {
+                    Console.WriteLine("\nWrong username or password, enter 0 to go back, or any key to try again");
+                    key = Console.ReadKey().KeyChar;
+                }
+            }
+
+            return passenger;
+        }
+
+        private static void LandPassenger()
+        {
+            int userOption;
+            OnlinePassenger? passenger = null;
+
+            do
+            {
+                userOption = GetUserOption("OnlinePassengerLanding", 0, 3);
+
+                if (userOption == 1)
+                {
+                    passenger = loginPassenger();
+                    if (passenger != null)
+                    {
+
+                    }
+                }
+                else if (userOption == 2)
+                {
+                    passenger = signupPassenger();
+                }
+            }
+            while (userOption != 0);
+        }
+
+        private static void LandEmployee()
+        {
+
+        }
+
+        private static void LandAdmin()
+        {
+
+        }
 
         private static void Main(string[] args)
         {
-            /*List<string> karim = new List<string>();
-            karim.Add("help");
-            karim.Add("jedl");
-
-            bool v = karim.Any("help");
-    */
-
-            string? username = Console.ReadLine();
-            string? password = Console.ReadLine();
-            int SSN = Convert.ToInt32(Console.ReadLine());
-
-            OnlinePassenger? op = Admin.loginPassenger(username!, password!);
-
-            if (op != null)
+            if (args.Length != 1)
             {
-                Console.WriteLine(op.auth);
-            }
-            else
-            {
-                op = OnlinePassenger.signup(SSN, username!, password!);
-                Console.WriteLine("Signed up");
+                throw new Exception("Specify user type only: passenger, employee, or admin");
             }
 
-            Admin admin = Admin.loginAdmin("admin", "admin")!;
-            Console.WriteLine(admin.auth);
-
-/*
-            Console.ReadLine($"Username: {0}\nPassword: {1}", username,password);*/
-
-
-
-
-
-            /*List<int> numbers = new List<int> { 1, 2 };
-            bool hasElements = numbers.Any();
-*/
-
-            /*Console.WriteLine("The list {0} empty.", hasElements ? "is not" : "is");
-*/
-
-            /*        int userOption = -1;
-
-                    while (userOption != 0)
-                    {
-                        PrintMenu("main");
-
-                        userOption = GetUserOption(0, 2);
-
-                        Console.WriteLine(userOption);
-
-                        if (userOption == 1)
-                        {
-                            PrintMenu("login");
-                            Console.Read();
-                        }
-                    }
-            */
+            if (args[0] == "passenger")
+            {
+                LandPassenger();
+            }
+            else if (args[0] == "employee")
+            {
+                LandEmployee();
+            }
+            else if (args[0] == "admin")
+            {
+                LandAdmin();
+            }
         }
     }
 }
